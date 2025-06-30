d01ce9dcac2ef0a951da1e241c07199f08ce4fb8
小马同学 <89441679+m2075498242@users.noreply.github.com>
2025-06-30 15:20:23 +0800

    chore:globals.css内容更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1> */}
       <Button variant="outline">Button</Button>
       <button style={{
-        backgroundColor: "#f60", // bg-blue-500
+        backgroundColor: "#f20", // bg-blue-500
         color: "white",
         fontWeight: "bold",
         padding: "0.5rem 1rem", // py-2 px-4
         borderRadius: "0.25rem", // rounded
         marginLeft: "0.5rem",
       }}>
-        原生Button测试
+        原生Button
       </button>
     </div>
   );
